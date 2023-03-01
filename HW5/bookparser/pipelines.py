ce106142858734994f6d71490168d984ef459a33
ColShul <soshulgin@gmail.com>
2023-03-01 11:47:23 +0300

    Добавил файлы с результатами выполнения д/з по теме 5 курса 7 "Парсинг"

---
+++ HW5/bookparser/pipelines.py
@@ -0,0 +1,37 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from pymongo import MongoClient
+
+
+class BookparserLabirintPipeline:
+    """Labirint.ru parser pipeline."""
+    def __init__(self):
+        """Constructor."""
+        client = MongoClient('localhost', 27017)
+        self.mongo_base = client.books
+
+
+    def process_item(self, item, spider):
+        """Process item."""
+        collection = self.mongo_base[spider.name]
+
+        item['own_price'] = int(item['own_price'])
+
+        if item['sale_price']:
+            item['sale_price'] = int(item['sale_price'])
+
+        if item['rating']:
+            item['rating'] = float(item['rating'])
+
+        collection.update_one(item, {'$setOnInsert': item}, upsert=True)
+
+        return item
+
+
+
+
---
+++ HW5/bookparser/spiders/labirintru.py
@@ -0,0 +1,44 @@
+import scrapy
+from scrapy.http import HtmlResponse
+
+# Подключаем файл items.py и импортировали поля
+from bookparser.items import BookparserLabirintItem
+
+
+class LabirintruSpider(scrapy.Spider):
+
+    name = 'labirintru'
+    custom_settings = {
+        'ITEM_PIPELINES': {'bookparser.pipelines.BookparserLabirintPipeline': 300}
+    }
+    allowed_domains = ['labirint.ru']
+
+    # Будем собирать информацию о книгах по теме
+    # "Машинное обучение. Анализ данных"
+
+    start_urls = ['https://www.labirint.ru/genres/3079/?page=1']
+
+    def parse(self, response):
+        """Novelty page parser."""
+        links = response.xpath('//a[@class="product-title-link"]/@href').extract()
+        for link in links:
+            yield response.follow('https://www.labirint.ru' + link, callback=self.parse_item)
+
+        next_page = response.xpath('//a[@class="pagination-next__text"]/@href').extract_first()
+        if next_page:
+            yield response.follow('https://www.labirint.ru/genres/3079/' + next_page, callback=self.parse)
+
+
+    def parse_item(self, response:HtmlResponse):
+        """Book item parser."""
+        title = response.xpath('//h1/text()').extract_first()
+        link = response.url
+        author = response.xpath('//a[@data-event-label="author"]/text()').extract_first()
+        own_price = response.xpath('//span[@class="buying-price-val-number"]/text()').extract_first()
+        if not own_price:
+            own_price = response.xpath('//span[@class="buying-priceold-val-number"]/text()').extract_first()
+        sale_price = response.xpath('//span[@class="buying-pricenew-val-number"]/text()').extract_first()
+        rating = response.xpath('//div[@id="rate"]/text').extract_first()
+
+        yield BookparserLabirintItem(title=title, link=link, author=author, own_price=own_price, sale_price=sale_price,
+                                     rating=rating)
